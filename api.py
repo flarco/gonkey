fa095a4b7da7b9a2c3fd01d7d3f3a4446da9b168
flarco <flarco@gmail.com>
2016-10-01 18:28:43 -0400

    INIT

--- api.py
+++ api.py
@@ -1,9 +1,19 @@
+import yaml, os
+
 from gmusicapi import Mobileclient
-import yaml
 
-
+DIR = os.path.dirname(os.path.realpath(__file__))
 
 # Get credentials
+with open(DIR + '/creds.yml') as creds_file:
+  cred = yaml.load(creds_file)
+
 
 api = Mobileclient()
-api.login('user@gmail.com', 'my-password', Mobileclient.FROM_MAC_ADDRESS)
+logged_in = api.login(
+  cred['google_email'],
+  cred['google_password'],
+  Mobileclient.FROM_MAC_ADDRESS
+)
+
+print(logged_in)
