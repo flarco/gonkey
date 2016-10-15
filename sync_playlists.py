cad4cac8c167170b6addd80abbf3ab4fa4726218
flarco <flarco@gmail.com>
2016-10-14 20:44:17 -0400

    Added some messages.

--- sync_playlists.py
+++ sync_playlists.py
@@ -2,8 +2,18 @@ import sys
 
 from google_music import GMusic
 from media_monkey import MM
-from helpers import settings
+from helpers import settings, log
 
+log('Start!')
+
+log('Loading MM.')
 mm = MM(settings)
+
+log('Loading GM.')
 gmusic = GMusic(settings, mm)
-gmusic.add_all_playlists()
+# gmusic.add_all_playlists()
+
+log('Syncing playlists!')
+gmusic.sync_all_playlists()
+
+log('End!')
