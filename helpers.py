8ac4906d0fb49702a5ad0f3a66bd222e4affb4c7
flarco <flarco@gmail.com>
2016-10-14 20:44:02 -0400

    Disable logger. Add YAML unicode support.

--- helpers.py
+++ helpers.py
@@ -1,14 +1,25 @@
 import yaml, os
+from yaml import Loader, SafeLoader
 import logging, datetime
 
-logger = logging.getLogger()
-handler = logging.StreamHandler()
-formatter = logging.Formatter('%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
-handler.setFormatter(formatter)
-logger.addHandler(handler)
-logger.setLevel(logging.INFO)
+# logger = logging.getLogger()
+# handler = logging.StreamHandler()
+# formatter = logging.Formatter('%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+# handler.setFormatter(formatter)
+# logger.addHandler(handler)
+# logger.setLevel(logging.INFO)
 
 DIR = os.path.dirname(os.path.realpath(__file__))
+
+
+def construct_yaml_str(self, node):
+    # Override the default string handling function
+    # to always return unicode objects
+    return self.construct_scalar(node)
+
+Loader.add_constructor(u'tag:yaml.org,2002:str', construct_yaml_str)
+SafeLoader.add_constructor(u'tag:yaml.org,2002:str', construct_yaml_str)
+
 
 class dict2(dict):
   """ Dict with attributes getter/setter. """
@@ -24,7 +35,8 @@ def load_settings():
   return settings
 
 def log(text):
-  logger.info(text)
+  # logger.info(text)
+  print('{} >> {}'.format(datetime.datetime.now(), text))
 
 # Get credentials & settings
 settings = load_settings()
